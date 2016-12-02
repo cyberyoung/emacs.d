8d5580ec0570c9451e83ae9f1c393541a16be6d6
liyang <pku.liyang@gmail.com>
2016-12-02 08:35:58 +0800

    merge

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -8,4 +8,68 @@
 
 (setq lisp-indent-offset 2)
 
+(add-to-list 'auto-mode-alist '("\\.js$" . js-jsx-mode))
+
+;; copy region or whole line(global-set-key "\M-w"
+(global-set-key "\M-w"
+(lambda ()
+  (interactive)
+  (if mark-active
+      (kill-ring-save (region-beginning)
+      (region-end))
+    (progn
+     (kill-ring-save (line-beginning-position)
+     (line-end-position))
+     (message "copied line")))))
+
+
+;; kill region or whole line
+(global-set-key "\C-w"
+(lambda ()
+  (interactive)
+  (if mark-active
+      (kill-region (region-beginning)
+   (region-end))
+    (progn
+     (kill-region (line-beginning-position)
+  (line-end-position))
+     (message "killed line")))))
+
+;; Copy line from point to the end, exclude the line break
+(defun qiang-copy-line (arg)
+  "Copy lines (as many as prefix argument) in the kill ring"
+  (interactive "p")
+  (kill-ring-save (point)
+                  (line-end-position))
+                  ;; (line-beginning-position (+ 1 arg)))
+  (message "%d line%s copied" arg (if (= 1 arg) "" "s")))
+ 
+(global-set-key (kbd "M-k") 'qiang-copy-line)
+
 (provide 'init-local)
+
+;; ;; Smart copy, if no region active, it simply copy the current whole line
+;; (defadvice kill-line (before check-position activate)
+;;   (if (member major-mode
+;;               '(emacs-lisp-mode scheme-mode lisp-mode
+;;                                 c-mode c++-mode objc-mode js-mode
+;;                                 latex-mode plain-tex-mode))
+;;       (if (and (eolp) (not (bolp)))
+;;           (progn (forward-char 1)
+;;                  (just-one-space 0)
+;;                  (backward-char 1)))))
+;;  
+;; (defadvice kill-ring-save (before slick-copy activate compile)
+;;   "When called interactively with no active region, copy a single line instead."
+;;   (interactive (if mark-active (list (region-beginning) (region-end))
+;;                  (message "Copied line")
+;;                  (list (line-beginning-position)
+;;                        (line-beginning-position 2)))))
+;;  
+;; (defadvice kill-region (before slick-cut activate compile)
+;;   "When called interactively with no active region, kill a single line instead."
+;;   (interactive
+;;    (if mark-active (list (region-beginning) (region-end))
+;;      (list (line-beginning-position)
+;;            (line-beginning-position 2)))))
+ 
